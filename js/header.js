ad9e07192ccdd962dacf7681c70275a2d18a4c75
Jalal Naghiyev <jalal.naghiyev.06@gmail.com>
2021-12-29 04:40:14 +0300

    Rewrote header

--- js/header.js
+++ js/header.js
@@ -1,30 +1,18 @@
 const popupButton = document.getElementById('burger-menu')
 const popupToggle = document.getElementById('burger-menu-toggle')
 const popupNavigation = document.getElementById('popup-navigation')
-window.onload = function (){
-    if (window.innerWidth < 1000){
-        popupNavigation.style.display = 'block'
-    }
-}
-window.addEventListener('resize', function (){
-    if (window.innerWidth > 1000){
-        popupNavigation.style.display = 'none'
-    }
-    else {
-        popupNavigation.style.display = 'block'
-    }
-})
+const body = document.querySelector('body')
+
 popupButton.addEventListener('click', function (e){
     e.preventDefault()
     if (popupToggle.classList.contains('is-active')){
         popupToggle.classList.remove('is-active')
-        popupToggle.style.borderTop = '2.5px solid'
-        popupToggle.style.borderBottom = '2.5px solid'
-        popupNavigation.style.transform = 'translate(100% , 0)'
+        popupNavigation.classList.remove('is-active')
+        body.classList.remove('stop-scrolling')
     }
     else{
         popupToggle.classList.add('is-active')
-        popupToggle.style.border = '0'
-        popupNavigation.style.transform = 'none'
+        popupNavigation.classList.add('is-active')
+        body.classList.add('stop-scrolling')
     }
 })
